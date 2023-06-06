958164666afd005d25b3ae910cb6a2cc1737242e
Ven <56719881+ven2day@users.noreply.github.com>
2023-06-06 19:18:18 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Jeevani Rao',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Jeevani Rao is a Java Full Stack Developer who specializes in building (and occasionally designing) exceptional backend software tools using Java and Spring (Spring Boot).',
+    siteUrl: 'https://g1e.me', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@itsjeevani',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,

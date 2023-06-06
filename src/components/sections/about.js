6b667a9b5cb9133a150a4438bffb0173030d2f75
Ven <56719881+ven2day@users.noreply.github.com>
2023-06-07 01:04:58 +0200

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -141,24 +141,15 @@ const About = () => {
               technologies such as Java, Spring Boot, Hibernate ,React, JavaScript, CSS, and SQL/NoSQL.
             </p>
 
-//             <p>
-//               Fast-forward to today, and I’ve had the privilege of working at{' '}
-//               <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-//               <a href="https://starry.com/">a start-up</a>,{' '}
-//               <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-//               <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-//               main focus these days is building accessible, inclusive products and digital
-//               experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-//               clients.
-//             </p>
-
             <p>
-              I also recently completed {' '}
+              I am a vmware certified {' '}
               <a href="https://www.vmware.com/learning/certification/spring-certified-pro.html">
-                Spring Certified Professional from VMware
+                Spring Certified Professional
               </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              and {' '}
+              <a href="https://www.vmware.com/learning/certification/spring-certified-pro.html">
+                Oracle Certified Professional - [Java 8] - Programmer
+              </a>{' '}.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
